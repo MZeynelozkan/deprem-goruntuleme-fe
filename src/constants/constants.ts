43fdb837807813a3ee0f61b7234c9b358c9c49ed
MZeynelozkan <zeyzey4340@gmail.com>
2024-10-11 12:10:29 +0300

    select button denemeleri

---
+++ src/constants/constants.ts
@@ -0,0 +1,274 @@
+export const countries = [
+  {
+    name: "Afganistan",
+    cities: ["Kabil", "Kandahar", "Herat"],
+  },
+  {
+    name: "Arnavutluk",
+    cities: ["Tiran", "Dıraç", "Vlora"],
+  },
+  {
+    name: "Cezayir",
+    cities: ["Cezayir", "Oran", "Constantine"],
+  },
+  {
+    name: "Andorra",
+    cities: ["Andorra la Vella", "Escaldes-Engordany"],
+  },
+  {
+    name: "Angola",
+    cities: ["Luanda", "Benguela", "Huambo"],
+  },
+  {
+    name: "Arjantin",
+    cities: ["Buenos Aires", "Córdoba", "Rosario"],
+  },
+  {
+    name: "Avustralya",
+    cities: ["Sydney", "Melbourne", "Brisbane"],
+  },
+  {
+    name: "Avusturya",
+    cities: ["Viyana", "Salzburg", "Graz"],
+  },
+  {
+    name: "Azerbaycan",
+    cities: ["Bakü", "Gence", "Sumgayıt"],
+  },
+  {
+    name: "Bahama",
+    cities: ["Nassau", "Freeport"],
+  },
+  {
+    name: "Bahreyn",
+    cities: ["Manama", "Riffa"],
+  },
+  {
+    name: "Bangladeş",
+    cities: ["Dakha", "Chittagong", "Khulna"],
+  },
+  {
+    name: "Barbados",
+    cities: ["Bridgetown", "Holetown"],
+  },
+  {
+    name: "Belçika",
+    cities: ["Brüksel", "Antwerp", "Gent"],
+  },
+  {
+    name: "Bolivya",
+    cities: ["La Paz", "Santa Cruz", "Cochabamba"],
+  },
+  {
+    name: "Bosna-Hersek",
+    cities: ["Saraybosna", "Banja Luka", "Mostar"],
+  },
+  {
+    name: "Brezilya",
+    cities: ["Rio de Janeiro", "Sao Paulo", "Brasilia"],
+  },
+  {
+    name: "Bulgaristan",
+    cities: ["Sofya", "Plovdiv", "Varna"],
+  },
+  {
+    name: "Danimarka",
+    cities: ["Kopenhag", "Aarhus", "Odense"],
+  },
+  {
+    name: "Dominika",
+    cities: ["Roseau"],
+  },
+  {
+    name: "Dominik Cumhuriyeti",
+    cities: ["Santo Domingo", "Santiago", "La Romana"],
+  },
+  {
+    name: "Ekvador",
+    cities: ["Quito", "Guayaquil", "Cuenca"],
+  },
+  {
+    name: "El Salvador",
+    cities: ["San Salvador", "Santa Ana", "San Miguel"],
+  },
+  {
+    name: "Endonezya",
+    cities: ["Cakarta", "Surabaya", "Bandung"],
+  },
+  {
+    name: "Estonya",
+    cities: ["Tallinn", "Tartu", "Narva"],
+  },
+  {
+    name: "Fas",
+    cities: ["Rabat", "Casablanca", "Marrakech"],
+  },
+  {
+    name: "Fiji",
+    cities: ["Suva", "Nadi"],
+  },
+  {
+    name: "Finlandiya",
+    cities: ["Helsinki", "Espoo", "Tampere"],
+  },
+  {
+    name: "Fransa",
+    cities: ["Paris", "Lyon", "Marsilya"],
+  },
+  {
+    name: "Gana",
+    cities: ["Accra", "Kumasi", "Takoradi"],
+  },
+  {
+    name: "Gine",
+    cities: ["Conakry", "Nzérékoré"],
+  },
+  {
+    name: "Güney Afrika",
+    cities: ["Cape Town", "Johannesburg", "Durban"],
+  },
+  {
+    name: "Güney Kore",
+    cities: ["Seul", "Busan", "Incheon"],
+  },
+  {
+    name: "Hindistan",
+    cities: ["Delhi", "Mumbai", "Bangalore"],
+  },
+  {
+    name: "Hırvatistan",
+    cities: ["Zagreb", "Split", "Dubrovnik"],
+  },
+  {
+    name: "Honduras",
+    cities: ["Tegucigalpa", "San Pedro Sula"],
+  },
+  {
+    name: "Irak",
+    cities: ["Bağdat", "Basra", "Erbil"],
+  },
+  {
+    name: "İran",
+    cities: ["Tahran", "Isfahan", "Şiraz"],
+  },
+  {
+    name: "İngiltere",
+    cities: ["Londra", "Manchester", "Birmingham"],
+  },
+  {
+    name: "İspanya",
+    cities: ["Madrid", "Barselona", "Valencia"],
+  },
+  {
+    name: "İsrail",
+    cities: ["Tel Aviv", "Kudüs", "Haifa"],
+  },
+  {
+    name: "İsveç",
+    cities: ["Stockholm", "Göteborg", "Malmö"],
+  },
+  {
+    name: "İtalya",
+    cities: ["Roma", "Milano", "Napoli"],
+  },
+  {
+    name: "Japonya",
+    cities: ["Tokyo", "Osaka", "Kyoto"],
+  },
+  {
+    name: "Kanada",
+    cities: ["Toronto", "Vancouver", "Montreal"],
+  },
+  {
+    name: "Karadağ",
+    cities: ["Podgorica", "Niksic"],
+  },
+  {
+    name: "Katar",
+    cities: ["Doha", "Al Wakrah"],
+  },
+  {
+    name: "Kazakhstan",
+    cities: ["Nur-Sultan", "Almatı"],
+  },
+  {
+    name: "Kırgızistan",
+    cities: ["Bişkek", "Oş"],
+  },
+  {
+    name: "Kıbrıs",
+    cities: ["Lefkoşa", "Girne"],
+  },
+  {
+    name: "Kolombiya",
+    cities: ["Bogota", "Medellin", "Cali"],
+  },
+  {
+    name: "Küba",
+    cities: ["Havana", "Santiago de Cuba"],
+  },
+  {
+    name: "Lübnan",
+    cities: ["Beyrut", "Tripoli"],
+  },
+  {
+    name: "Malavi",
+    cities: ["Lilongwe", "Blantyre"],
+  },
+  {
+    name: "Maldivler",
+    cities: ["Malé", "Addu City"],
+  },
+  {
+    name: "Malezya",
+    cities: ["Kuala Lumpur", "George Town"],
+  },
+  {
+    name: "Meksika",
+    cities: ["Mexico City", "Guadalajara", "Monterrey"],
+  },
+  {
+    name: "Nijerya",
+    cities: ["Lagos", "Abuja"],
+  },
+  {
+    name: "Norveç",
+    cities: ["Oslo", "Bergen", "Stavanger"],
+  },
+  {
+    name: "Portekiz",
+    cities: ["Lizbon", "Porto"],
+  },
+  {
+    name: "Rusya",
+    cities: ["Moskova", "St. Petersburg", "Novosibirsk"],
+  },
+  {
+    name: "Şili",
+    cities: ["Santiago", "Valparaíso"],
+  },
+  {
+    name: "Tayland",
+    cities: ["Bangkok", "Chiang Mai"],
+  },
+  {
+    name: "Türkiye",
+    cities: ["Ankara", "İstanbul", "İzmir"],
+  },
+  {
+    name: "Ukrayna",
+    cities: ["Kiev", "Lviv", "Odessa"],
+  },
+  {
+    name: "ABD",
+    cities: ["New York", "Los Angeles", "Chicago"],
+  },
+  {
+    name: "Zambiya",
+    cities: ["Lusaka", "Ndola"],
+  },
+  {
+    name: "Zimbabve",
+    cities: ["Harare", "Bulawayo"],
+  },
+];
--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,1 +1,6 @@
 export type Position = [number, number];
+
+export type countriesType = {
+  name: string;
+  cities: string[];
+};
