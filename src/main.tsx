b4a149a3591e91d21048db19ec75e196c3dd9e90
MZeynelozkan <zeyzey4340@gmail.com>
2024-10-23 00:18:55 +0300

    update

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,13 @@ import store from "./store/store.ts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5000,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
