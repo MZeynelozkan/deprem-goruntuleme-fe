26cad0d551b3b061c1ad005c7b7e4cad79a4d532
MZeynelozkan <zeyzey4340@gmail.com>
2024-10-24 10:18:05 +0300

    update

--- src/pages/drawRectangle/DrawRectanglePage.tsx
+++ src/pages/drawRectangle/DrawRectanglePage.tsx
@@ -1,6 +1,8 @@
 import DrawRectangle from "@/components/leaflet/DrawRectangle";
 import { getAllCities } from "@/services/getAPI";
 import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
 const DrawRectanglePage = () => {
   const { data: cities } = useQuery<any[]>({
@@ -8,7 +10,22 @@ const DrawRectanglePage = () => {
     queryFn: () => getAllCities(),
   });
 
-  return <DrawRectangle cities={cities} />;
+  // Sayfa yüklendiğinde bildirim göster
+  useEffect(() => {
+    toast(
+      "Haritanın istediğiniz bölgesine tıklayın. İlk tıklamada başlangıç noktası, ikinci tıklamada bitiş noktası alarak dikdörtgen çizin. Kalan aralıklardaki şehirleri göreceksiniz.",
+      {
+        duration: 5000, // Bildirim 5 saniye boyunca gösterilecek
+      }
+    );
+  }, []);
+
+  return (
+    <>
+      <Toaster /> {/* Toast bildirimlerini göstermek için eklenir */}
+      <DrawRectangle cities={cities} />
+    </>
+  );
 };
 
 export default DrawRectanglePage;
